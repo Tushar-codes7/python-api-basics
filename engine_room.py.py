d27f674c91106289fff55602d7512c82df7c54f3
Tushar-codes7 <tusharnahata7@gmail.com>
2026-02-04 12:49:29 +0530

    added part1 code

---
+++ engine_room.py.py
@@ -0,0 +1,30 @@
+# print hello world
+Msg = "Hello world!"
+print(Msg)
+
+# checking datatype of Msg
+print(type(Msg))
+
+# checking data type of nums
+numOne = 3
+numTwo = 3.0
+print(type(numOne))
+print(type(numTwo))
+
+# checking datatype of False
+val = False
+print(type(val))
+
+# checking nums and false datatypes
+print(numOne)
+print(numTwo)
+print(val)
+
+# printing nums and thier datatypes
+num1 = 3
+num2 = 5
+print(num1, num2)
+print(type(num1), type(num2))
+
+# performing add operation
+add = num1 + num2
